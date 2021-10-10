56bc6253f1cb44959cc0fd60aa35c1ddcab0af5c
renjval12 <reneejoyvalentine3@gmail.com>
2021-10-10 09:25:29 -0400

    update index.html & Readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# AffirmationsAPI
+# Affirmations API
+## About Affirmations API
+Affirmations REST API allows developers to access and integrate the functionality of Affirmations with other applications. The API retrieves a list of affirmations to motivate all those who read them.
+## Use
+Affirmations is for a developer who wants an API that provides a list of positive affirmations, and the ability to randomize the output of this list, to motivate users who use their applications.
+## URL
+        GET: https://affirmationsapi.herokuapp.com 
+
+## Parameters
+- /api:
+    - Retrieves a list of positive affirmations for developers to integrate into their applications.
+- /api/random-affirmation:
+    - Generates a random affirmation for the developer to utilize.
+
+
--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const path = require('path');
 const data = require('./api/affirmations.json')
 
 const express = require('express')
+const rateLimit = require('express-rate-limit')
 const app = express()
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000
+
+const apiRequestLimiter = rateLimit({
+    windowMs: 1 * 60 * 10000,
+    max: 10
+})
+app.use(apiRequestLimiter)
 
 app.get('/', async (req, res) => {
     try {
@@ -26,6 +33,18 @@ app.get('/api', async (req, res) => {
         })
     }
 });
+app.get('/api/random-affirmation', async (req, res) => {
+    try {
+        res.status(200).send(
+            data[Math.round(Math.random() * data.length)]
+        )
+    } catch (err) {
+        res.status(500).send({
+            message: err.message
+        })
+
+    }
+})
 app.get('/api/random-affirmation', async (req, res) => {
     try {
         res.status(200).send(
